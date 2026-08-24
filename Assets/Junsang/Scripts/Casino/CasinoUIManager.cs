6f2886125d48cf7692ed7e8c75de7617eacdee40
Nora <gimnora01@gmail.com>
2026-08-24 15:37:38 +0900

    Fix | 수정
    - 은행: 가로 폭 조정
    - 도박: 충전 버튼 클릭시 충전 페이지로 넘어가게 설정, 원화로 변경

--- Assets/Junsang/Scripts/Casino/CasinoUIManager.cs
+++ Assets/Junsang/Scripts/Casino/CasinoUIManager.cs
@@ -12,8 +12,8 @@ namespace Dobak.App.Casino
         [Header("카지노 메뉴")]
         [SerializeField] private Button menu_homeButton;
         [SerializeField] private Button menu_slotMachineButton;
-        [SerializeField] private Button menu_rechargeButton;
-        [SerializeField] private Button menu_myPageButton;
+        [SerializeField] private Button[] menu_rechargeButtons;
+
         [SerializeField] private TMP_Text casinoCashText;
 
         [Header("홈")]
@@ -25,8 +25,6 @@ namespace Dobak.App.Casino
         [SerializeField] private Button _10DollorButton;
         [SerializeField] private Button _100DollorButton;
         [SerializeField] private Button _1000DollorButton;
-        [SerializeField] private Button _10000DollorButton;
-        [SerializeField] private Button _100000DollorButton;
 
         [Header("패널")]
         [SerializeField] private GameObject homePanel;
@@ -43,15 +41,16 @@ namespace Dobak.App.Casino
         {
             menu_homeButton.onClick.AddListener(OnHomeButtonClicked);
             menu_slotMachineButton.onClick.AddListener(OnSlotMachineButtonClicked);
-            menu_rechargeButton.onClick.AddListener(OnRechargeButtonClicked);
-            menu_myPageButton.onClick.AddListener(OnProfileButtonClicked);
+
+            foreach(var rechargeButton in menu_rechargeButtons)
+            {
+                rechargeButton.onClick.AddListener(OnRechargeButtonClicked);
+            }
 
             _1DollorButton.onClick.AddListener(On1DollarButtonClicked);
             _10DollorButton.onClick.AddListener(On10DollarButtonClicked);
             _100DollorButton.onClick.AddListener(On100DollarButtonClicked);
             _1000DollorButton.onClick.AddListener(On1000DollarButtonClicked);
-            _10000DollorButton.onClick.AddListener(On10000DollarButtonClicked);
-            _100000DollorButton.onClick.AddListener(On100000DollarButtonClicked);
 
             // CoinManager가 있을 때만 연결
             if (CoinManager.Instance != null)
@@ -70,19 +69,21 @@ namespace Dobak.App.Casino
 
             menu_homeButton.onClick.RemoveListener(OnHomeButtonClicked);
             menu_slotMachineButton.onClick.RemoveListener(OnSlotMachineButtonClicked);
-            menu_rechargeButton.onClick.RemoveListener(OnRechargeButtonClicked);
-            menu_myPageButton.onClick.RemoveListener(OnProfileButtonClicked);
+
+            foreach(var rechargeButton in menu_rechargeButtons)
+            {
+                rechargeButton.onClick.RemoveListener(OnRechargeButtonClicked);
+            }
+
             _1DollorButton.onClick.RemoveListener(On1DollarButtonClicked);
             _10DollorButton.onClick.RemoveListener(On10DollarButtonClicked);
             _100DollorButton.onClick.RemoveListener(On100DollarButtonClicked);
             _1000DollorButton.onClick.RemoveListener(On1000DollarButtonClicked);
-            _10000DollorButton.onClick.RemoveListener(On10000DollarButtonClicked);
-            _100000DollorButton.onClick.RemoveListener(On100000DollarButtonClicked);
         }
 
         private void UpdateDisplay(int casinoCash)
         {
-            casinoCashText.text = $"Cash: ${casinoCash}";
+            casinoCashText.text = $"{casinoCash} 원";
         }
 
         private void Init()
@@ -136,7 +137,7 @@ namespace Dobak.App.Casino
 
             if (CoinManager.Instance.TryChargeToCasino(cash, out ChargeToCasinoFailureReason failureReason))
             {
-                ShowPopup("출금 완료", $"${cash}가 카지노 캐시로 이동했습니다.");
+                ShowPopup("출금 완료", $"카지노에 {cash}원을 충전했습니다.");
                 return;
             }
 
@@ -172,16 +173,12 @@ namespace Dobak.App.Casino
             Debug.LogWarning($"{nameof(CasinoUIManager)} could not find a NotificationManager.");
         }
 
-        private void On1DollarButtonClicked() => OnCashButtonClicked(1);
+        private void On1DollarButtonClicked() => OnCashButtonClicked(1000);
 
-        private void On10DollarButtonClicked() => OnCashButtonClicked(10);
+        private void On10DollarButtonClicked() => OnCashButtonClicked(5000);
 
-        private void On100DollarButtonClicked() => OnCashButtonClicked(100);
+        private void On100DollarButtonClicked() => OnCashButtonClicked(10000);
 
-        private void On1000DollarButtonClicked() => OnCashButtonClicked(1000);
-
-        private void On10000DollarButtonClicked() => OnCashButtonClicked(10000);
-
-        private void On100000DollarButtonClicked() => OnCashButtonClicked(100000);
+        private void On1000DollarButtonClicked() => OnCashButtonClicked(50000);
     }
 }
